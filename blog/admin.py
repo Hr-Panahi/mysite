f166cb7c3d81676e9997ef48c8251cf155b265f3
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-13 23:41:04 +0430

    Model + Admin
    created models/admin for blog and website contact

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = "-empty-"
+    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
+    list_filter = ('status',) #we use comma to indicate that its a tupple
+    ordering = ('-created_date',) # "-" doing the reverse order
+    search_fields = ('title', 'content')
+# instead of line 6 we can use the line below:
+#admin.site.register(Post.PostAdmin)
