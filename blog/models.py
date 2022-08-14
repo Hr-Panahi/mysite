28b40c8875066b4b7ea64383c55cdd080742a3a1
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-14 19:42:39 +0430

    counted_views + published posts

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,9 @@ class Post(models.Model):
     content = models.TextField()
     # tag
     # category
+
     counted_views = models.IntegerField(default=0)
+
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
+from datetime import datetime
+from django.db.models import F
 
 # Create your views here.
+
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(published_date__lte=datetime.today())
+    context = {'posts':posts}
+    Post.objects.filter(status=True).update(counted_views=F('counted_views')+1)
+    return render(request, 'blog/blog-home.html',context)
 
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    posts = Post.objects.filter(published_date__lte=datetime.today())
+    context = {'posts':posts}
+    Post.objects.filter(status=True).update(counted_views=F('counted_views')+1)
+    return render(request, 'blog/blog-single.html',context)
+
+def test(request,pid):
+    #post = Post.objects.get(id=pid)     
+    post = get_object_or_404(Post,pk=pid)
+    context = {'post':post}
+    return render(request, 'test.html',context)
