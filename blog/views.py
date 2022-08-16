2c15f854d1b804e701759401bed356edc96ff29e
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-16 14:06:04 +0430

    fixed single page urls

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,17 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-from datetime import datetime
-from django.db.models import F
+from django.utils import timezone
 
 # Create your views here.
 
 def blog_view(request):
-    posts = Post.objects.filter(published_date__lte=datetime.today())
+    posts = Post.objects.filter(published_date__lte=timezone.now())
     context = {'posts':posts}
-    Post.objects.filter(status=True).update(counted_views=F('counted_views')+1)
     return render(request, 'blog/blog-home.html',context)
 
-def blog_single(request):
-    posts = Post.objects.filter(published_date__lte=datetime.today())
-    context = {'posts':posts}
-    Post.objects.filter(status=True).update(counted_views=F('counted_views')+1)
-    return render(request, 'blog/blog-single.html',context)
-
-def test(request,pid):
-    #post = Post.objects.get(id=pid)     
+def blog_single(request,pid):
     post = get_object_or_404(Post,pk=pid)
+    post.counted_views +=1
+    post.save()
     context = {'post':post}
-    return render(request, 'test.html',context)
+    return render(request, 'blog/blog-single.html', context)
