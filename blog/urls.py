856c20dc3e09f6bd676d7699132e36c4e2d170d9
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-16 13:40:26 +0430

    Fixed timezone and counted views

--- blog/urls.py
+++ blog/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from blog.views import *
 
 
+
 app_name = 'blog'
 
 urlpatterns = [
-
     path('', blog_view, name='index'),
-    path('single', blog_single, name='single'),
+    path('<int:pid>' ,blog_single,name='single')
 ]
