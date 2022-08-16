df2ca19752ed9645fd5e0faf47298c4f87b8559d
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-16 15:14:20 +0430

    Author + Images models

--- blog/admin.py
+++ blog/admin.py
@@ -7,8 +7,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = "-empty-"
-    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
-    list_filter = ('status',) #we use comma to indicate that its a tupple
+    list_display = ('title','author', 'counted_views', 'status', 'published_date', 'created_date')
+    list_filter = ('status','author') #we use comma to indicate that its a tupple
     ordering = ('-created_date',) # "-" doing the reverse order
     search_fields = ('title', 'content')
 # instead of line 6 we can use the line below:
--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,17 @@
 from pyexpat import model
 from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    # image
-    # author
+    image = models.ImageField(upload_to = 'blog/',default='blog/default.jpg')
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
     # category
-
     counted_views = models.IntegerField(default=0)
-
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
