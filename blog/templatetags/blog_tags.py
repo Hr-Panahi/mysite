2d53a12ec2f7d6f2b904fd5f1f51e657046f5e2a
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-20 22:01:46 +0430

    Inclusion tag and latest posts
    used inclusion tag and included latest posts with it in blog home and single

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -11,3 +11,12 @@ def function():
 def function():
     posts = Post.objects.filter(status=1)
     return posts
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:arg] + "..."
+
+@register.inclusion_tag('blog/blog-latest-posts.html')
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
+    return {'posts':posts}
