5aa220b3adc8954a6b47ad12d663e89c5d8e96c6
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-22 15:27:25 +0430

    Post filter by author

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,12 @@ from django.utils import timezone
 
 # Create your views here.
 
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(published_date__lte=timezone.now(),status=1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html',context)
 
@@ -21,5 +25,12 @@ def blog_single(request,id):
 
 def test(request):
     return render(request,'test.html')
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status=1)
+    posts = posts.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
+
 
 
