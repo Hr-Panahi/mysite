fcb71faadca6b045ec1df20202ca52ed45a46e91
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-24 16:47:38 +0430

    Forms
    +Contact form
    +Newsletter form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from website.models import Contact
+from website.forms import NameForm, ContactForm, NewsletterForm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -8,7 +10,30 @@ def about_view(request):
     return render(request, 'website/about.html')
 
 def contact_view(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    
+    form = ContactForm()
+    return render(request, 'website/contact.html', {'form': form})
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
 
 def test_view(request):
-    return render(request, 'website/test.html', {'name':'hamid','lastname':'panahi'})
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Success')
+        else:
+            return HttpResponse('error')
+    form = ContactForm()
+    return render(request, 'test.html', {'form':form})
