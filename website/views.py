c6a39379a216eedf0efebe7659fd302a603056a3
Hr-Panahi <110527736+Hr-Panahi@users.noreply.github.com>
2022-08-24 21:10:09 +0430

    Messages in template
    Show messages and errors in Django templates.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,9 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse
 from website.models import Contact
 from website.forms import NameForm, ContactForm, NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -14,7 +16,10 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
-    
+            messages.add_message(request, messages.SUCCESS, 'Your ticket submitted successfully.')
+        else:
+            messages.add_message(request, messages.ERROR, "Your ticket didn't submit.")
+
     form = ContactForm()
     return render(request, 'website/contact.html', {'form': form})
 
